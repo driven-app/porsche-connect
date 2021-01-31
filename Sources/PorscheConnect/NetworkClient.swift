3c434b755527860ab2b561d639d99fe8c4023458
Damien Glancy <damien.glancy@icloud.com>
2021-01-31 09:56:56 +0000

    Building out Network Client

--- Sources/PorscheConnect/NetworkClient.swift
+++ Sources/PorscheConnect/NetworkClient.swift
@@ -83,6 +83,12 @@ enum HTTPStatusCodes: Int, Error {
   }
 }
 
+// MARK: - Typealias
+
+public typealias ResponseJson = Data
+
+// MARK: - Structs
+
 struct NetworkClient {
   let baseURL: URL
   let session: URLSession
@@ -96,4 +102,108 @@ struct NetworkClient {
     configuration.timeoutIntervalForRequest = timeoutIntervalForRequest
     self.session = URLSession(configuration: configuration)
   }
+  
+  // MARK: - Public
+  
+  func get<D: Decodable>(_ responseType: D.Type, endpoint: String, params: [String : String]? = nil, headers: [String : String]? = nil, completion: @escaping (D?, HTTPURLResponse?, Error?, ResponseJson?) -> Void) {
+    let url = baseURL.addEndpoint(endpoint: endpoint).addParams(params: params)
+    let request = self.createCommonRequest(url: url, method: RestMethod.get.rawValue, headers: headers, contentType: "application/json", bodyData: nil)
+    self.performRequest(responseType, request: request, completion: completion)
+  }
+  
+  // MARK: - Private
+  
+  private func createCommonRequest(url: URL, method: String, headers: [String : String]?, contentType: String, bodyData: Data?) -> URLRequest {
+    var request = URLRequest(url: url)
+    request.httpMethod = method
+    if let headers = headers {
+      for (key, value) in headers {
+        request.addValue(value, forHTTPHeaderField: key)
+      }
+    }
+    request.httpBody = bodyData
+    //    request.sign(key: apiKey, secret: apiSecret, contentType: contentType)
+    return request
+  }
+  
+  private func performRequest<D: Decodable>(_ responseType: D.Type, request: URLRequest, completion: @escaping (D?, HTTPURLResponse?, Error?, ResponseJson?) -> Void) {
+    let task = self.session.dataTask(with: request) { (data, urlResponse, error) in
+      let response = urlResponse as? HTTPURLResponse
+      
+      if let response = response {
+        if self.isErrorStatusCode(response) {
+          completion(nil, response, HTTPStatusCodes(rawValue: response.statusCode), nil)
+          return
+        }
+      }
+      
+      guard let data = data, error == nil, !data.isEmpty else {
+        completion(nil, response, error, nil)
+        return
+      }
+      
+      let decoder = JSONDecoder()
+      decoder.keyDecodingStrategy = .convertFromSnakeCase
+      
+      do {
+        let result = try decoder.decode(D.self, from: data)
+        completion(result, response, nil, data)
+      } catch {
+        completion(nil, response, error, nil)
+      }
+    }
+    
+    task.resume()
+  }
+  
+  private func isErrorStatusCode(_ response: HTTPURLResponse) -> Bool {
+    return response.statusCode >= 400
+  }
 }
+
+// MARK: - Extensions
+
+public extension URL {
+  func addEndpoint(endpoint: String) -> URL {
+    return self.appendingPathComponent(endpoint)
+  }
+  
+  func addParams(params: [String: String]?) -> URL {
+    guard let params = params else {
+      return self
+    }
+    
+    var urlComponents = URLComponents(url: self, resolvingAgainstBaseURL: true)!
+    var queryItems = [URLQueryItem]()
+    
+    let existingQueryItems = query?.components(separatedBy: "&").map({
+      $0.components(separatedBy: "=")
+    }).reduce(into: [URLQueryItem](), { queryItems, pair in
+      if pair.count == 2 {
+        queryItems.append(URLQueryItem(name: pair[0], value: pair[1]))
+      }
+    })
+    
+    if let existingQueryItems = existingQueryItems {
+      queryItems.append(contentsOf: existingQueryItems)
+    }
+    
+    for (key, value) in params.reversed() {
+      queryItems.append(URLQueryItem(name: key, value: value))
+    }
+    
+    urlComponents.queryItems = queryItems
+    
+    return urlComponents.url!
+  }
+}
+
+
+
+//let params = queryString.components(separatedBy: "&").map({
+//    $0.components(separatedBy: "=")
+//}).reduce(into: [String:String]()) { dict, pair in
+//    if pair.count == 2 {
+//        dict[pair[0]] = pair[1]
+//    }
+//}
--- Tests/PorscheConnectTests/NetworkClientTests.swift
+++ Tests/PorscheConnectTests/NetworkClientTests.swift
@@ -16,7 +16,28 @@ final class NetworkClientTests: XCTestCase {
   
   // MARK: - Tests
   
-  func testGet() {
-    
+  func testUrlExtensionAddEndpoint() {
+    let url = URL(string: "https://api.porsche.example")!.addEndpoint(endpoint: "endpoint")
+    XCTAssertEqual(URL(string: "https://api.porsche.example/endpoint"), url)
+  }
+  
+  func testUrlExtensionAddSingleParam() {
+    let url = URL(string: "https://api.porsche.example")!.addParams(params: ["param_key": "param_value"])
+    XCTAssertEqual(URL(string: "https://api.porsche.example?param_key=param_value"), url)
+  }
+  
+  func testUrlExtensionAddMultipleParams() {
+    let url = URL(string: "https://api.porsche.example")!.addParams(params: ["param_key_1": "param_value_1", "param_key_2": "param_value_2"])
+    XCTAssertEqual(URL(string: "https://api.porsche.example?param_key_1=param_value_1&param_key_2=param_value_2"), url)
+  }
+  
+  func testUrlExtensionAddMultipleParamsToUrlWithExistingParam() {
+    let url = URL(string: "https://api.porsche.example?param_key_1=param_value_1")!.addParams(params: ["param_key_2": "param_value_2"])
+    XCTAssertEqual(URL(string: "https://api.porsche.example?param_key_1=param_value_1&param_key_2=param_value_2"), url)
+  }
+  
+  func testUrlExtensionAddEndpointAndMultipleParams() {
+    let url = URL(string: "https://api.porsche.example")!.addEndpoint(endpoint: "endpoint").addParams(params: ["param_key_1": "param_value_1", "param_key_2": "param_value_2"])
+    XCTAssertEqual(URL(string: "https://api.porsche.example/endpoint?param_key_1=param_value_1&param_key_2=param_value_2"), url)
   }
 }
