bba5b944055ebdd2c59bf2cb5b9c6b9faba1fdc1
Damien Glancy <damien.glancy@icloud.com>
2021-02-08 00:05:22 +0000

    Extracting API Key from JWT

--- Sources/PorscheConnect/Models.swift
+++ Sources/PorscheConnect/Models.swift
@@ -1,8 +1,25 @@
 import Foundation
 
 public struct PorscheAuth: Codable {
-  let accessToken: String
-  let idToken: String
-  let tokenType: String
-  let expiresIn: Int
+  
+  // MARK: - Properties
+  
+  public let accessToken: String
+  public let idToken: String
+  public let tokenType: String
+  public let expiresIn: Int
+  public let apiKey: String?
+  
+  
+  public init?(accessToken: String, idToken: String, tokenType: String, expiresIn: Int) {
+    self.accessToken = accessToken
+    self.idToken = idToken
+    self.tokenType = tokenType
+    self.expiresIn = expiresIn
+    
+    let idTokenComponents = self.idToken.components(separatedBy: ".")
+    let decodedString = String(data: Data(base64Encoded: idTokenComponents[1]) ?? kBlankData, encoding: .utf8)!
+    let jwtDctionary = try! JSONSerialization.jsonObject(with: decodedString.data(using: .utf8)!, options: .mutableContainers) as! Dictionary<String, Any>
+    self.apiKey = jwtDctionary["aud"] as! String
+  }
 }
--- Tests/PorscheConnectTests/ModelsTests.swift
+++ Tests/PorscheConnectTests/ModelsTests.swift
@@ -2,9 +2,12 @@ import XCTest
 @testable import PorscheConnect
 
 final class ModelsTests: XCTestCase {
+  let porscheAuth = PorscheAuth(accessToken: "zVb3smCN32iOslsoXa7XIYPrenGz",
+                                idToken: "yJhbGciOiJSUzI1NiIsImtpZCI6IjE1bF9LeldTV08tQ1ZNdXdlTmQyMnMifQ.eyJzdWIiOiI4N3VnOGJobXZydnF5bTFrIiwiYXVkIjoiVFo0VmY1d25LZWlwSnh2YXRKNjBsUEhZRXpxWjRXTnAiLCJqdGkiOiI5NWhPT0ZlSDdzZW9yaVZ2bUNhTWdWIiwiaXNzIjoiaHR0cHM6XC9cL2xvZ2luLnBvcnNjaGUuY29tIiwiaWF0IjoxNjEyNzQwOTE2LCJleHAiOjE2MTI3NDEyMTYsInBpLnNyaSI6IkVYYjZSSlFpRWZLazNRZWk0Y1dyTWlwSmgxSSJ9.bVzapayesKjA85pRwVBZN_TfKzPNFTOb6nszPSWElMU2-MOzmJjy6dWHTjN3jCCx3Ui20XDwHkkDOdIUZqIQq6nve5ihbRlNi1ywrNiKKLOL7nmfzmM7yBPMZfwxtCP_-imypF_n19i1rZDkatIkW0Ejs7lcc0xRD9JewGMhfALqpFuOciIX3SIInHE56WSmTNyEB1LTNNLXiwaBWygPVbYDAYYc4u-w3V_GPZR3kTSTJjwnfXM9Qke6wBcoXDaON4_NfNcTQf0vXYwhC749dJd8Z2eDcRTl-Yl06BTHHTIL-yInfk8yjCO1iaCv01ROjK_nGAyPsOvUKtVgsaXxnw",
+                                tokenType: "Bearer",
+                                expiresIn: 7199)!
   
   func testPorscheAuthConstruction() {
-    let porscheAuth = PorscheAuth(accessToken: "a_test_access_token", idToken: "a_test_id_token", tokenType: "Bearer", expiresIn: 7199)
     XCTAssertNotNil(porscheAuth)
   }
   
@@ -20,5 +23,10 @@ final class ModelsTests: XCTestCase {
     XCTAssertEqual("eyJhbGciOiJSUzI1NiIsImtpZCI6IjE1bF9LeldTV08tQ1ZNdXdlTmQyMnMifQ", porscheAuth.idToken)
     XCTAssertEqual("Bearer", porscheAuth.tokenType)
     XCTAssertEqual(7199, porscheAuth.expiresIn)
-  }  
+  }
+  
+  func testDecodedJwtToken() {
+    XCTAssertNotNil(porscheAuth)
+    XCTAssertEqual("TZ4Vf5wnKeipJxvatJ60lPHYEzqZ4WNp", porscheAuth.apiKey)
+  }
 }
