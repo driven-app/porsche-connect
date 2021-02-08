8e4caae54045acdfc981225504b9895d89dfcf50
Damien Glancy <damien.glancy@icloud.com>
2021-02-08 00:31:47 +0000

    Handling api key as a computed property

--- Sources/PorscheConnect/Models.swift
+++ Sources/PorscheConnect/Models.swift
@@ -8,18 +8,24 @@ public struct PorscheAuth: Codable {
   public let idToken: String
   public let tokenType: String
   public let expiresIn: Int
-  public let apiKey: String?
   
+  public var apiKey: String? {
+    let idTokenComponents = idToken.components(separatedBy: ".")
+    
+    if let decodedString = String(data: Data(base64Encoded: idTokenComponents[1]) ?? kBlankData, encoding: .utf8),
+       let data = decodedString.data(using: .utf8),
+       let dict = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? Dictionary<String, Any>,
+       let apiKey = dict["aud"] as? String {
+      return apiKey
+    } else {
+      return nil
+    }
+  }
   
-  public init?(accessToken: String, idToken: String, tokenType: String, expiresIn: Int) {
+  public init(accessToken: String, idToken: String, tokenType: String, expiresIn: Int) {
     self.accessToken = accessToken
     self.idToken = idToken
     self.tokenType = tokenType
     self.expiresIn = expiresIn
-    
-    let idTokenComponents = self.idToken.components(separatedBy: ".")
-    let decodedString = String(data: Data(base64Encoded: idTokenComponents[1]) ?? kBlankData, encoding: .utf8)!
-    let jwtDctionary = try! JSONSerialization.jsonObject(with: decodedString.data(using: .utf8)!, options: .mutableContainers) as! Dictionary<String, Any>
-    self.apiKey = jwtDctionary["aud"] as! String
   }
 }
