bc58e47216d63de5df9faa161fa55f6724ed57a8
Damien Glancy <damien.glancy@icloud.com>
2021-02-13 06:44:19 +0000

    NPE fix

--- Sources/PorscheConnect/PorscheConnect+Portal.swift
+++ Sources/PorscheConnect/PorscheConnect+Portal.swift
@@ -4,7 +4,12 @@ public extension PorscheConnect {
   
   func vehicles(completion: @escaping (Result<([Vehicle]?, HTTPURLResponse?), Error>) -> Void) {
     executeWithAuth { [self] in
-      let headers = buildHeaders(accessToken: auth!.accessToken, apiKey: auth!.apiKey!, countryCode: environment.countryCode, languageCode: environment.languageCode)
+      guard let auth = auth, let apiKey = auth.apiKey else {
+        completion(.failure(PorscheConnectError.AuthFailure))
+        return
+      }
+      
+      let headers = buildHeaders(accessToken: auth.accessToken, apiKey: apiKey, countryCode: environment.countryCode, languageCode: environment.languageCode)
       
       networkClient.get([Vehicle].self, url: networkRoutes.vehiclesURL, headers: headers, jsonKeyDecodingStrategy: .useDefaultKeys) { result in
         completion(result)
