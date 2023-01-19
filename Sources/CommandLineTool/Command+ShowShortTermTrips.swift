f1381d72927ff75e9789268fad82d1a9f24a13c1
Damien Glancy <damien.glancy@icloud.com>
2023-01-19 18:10:45 +0000

    Adding show-short-term-trips command line

---
+++ Sources/CommandLineTool/Command+ShowShortTermTrips.swift
@@ -0,0 +1,55 @@
+import ArgumentParser
+import Foundation
+import PorscheConnect
+
+extension Porsche {
+  
+  struct ShowShortTermTrips: AsyncParsableCommand {
+    // MARK: - Properties
+    
+    @OptionGroup() var options: Options
+    
+    @Argument(help: ArgumentHelp(NSLocalizedString("Your vehicle VIN.", comment: "")))
+    var vin: String
+    
+    // MARK: - Lifecycle
+    
+    func run() async throws {
+      let porscheConnect = PorscheConnect(
+        username: options.username,
+        password: options.password,
+        environment: options.resolvedEnvironment
+      )
+      await callTripService(porscheConnect: porscheConnect, vin: vin)
+      dispatchMain()
+    }
+    
+    // MARK: - Private functions
+    
+    private func callTripService(porscheConnect: PorscheConnect, vin: String) async {
+      
+      do {
+        let result = try await porscheConnect.trips(vin: vin)
+        printTrips(result.trips)
+        Porsche.ShowShortTermTrips.exit()
+      } catch {
+        Porsche.ShowShortTermTrips.exit(withError: error)
+      }
+    }
+    
+    private func printTrips(_ trips: [Trip]?) {
+      guard let trips = trips else { return }
+      
+      trips.enumerated().forEach { (index, trip) in
+        printTrip(trip, at: index)
+      }
+    }
+    
+    private func printTrip(_ trip: Trip, at index: Int) {
+      let output = NSLocalizedString(
+        "#\(index+1) => Trip ID: \(trip.id); Timestamp: \(trip.timestamp); Distance: \(trip.tripMileage.valueInKilometers) km; Average speed: \(trip.averageSpeed.valueInKmh) km/h; EV consumption: \(trip.averageElectricEngineConsumption.valueKwhPer100Km) kWh/100km",
+        comment: "") //TODO: Implement locales for units and timestamp
+      print(output)
+    }
+  }
+}
