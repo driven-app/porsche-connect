128698738c721ae97b271b635d94f2c3c9918523
Jeff Verkoeyen <jverkoey@gmail.com>
2023-01-20 20:29:42 -0800

    Make RemoteCommandStatus accept a vin instead of a Vehicle.

--- Sources/PorscheConnect/PorscheConnect+RemoteCommandStatus.swift
+++ Sources/PorscheConnect/PorscheConnect+RemoteCommandStatus.swift
@@ -3,7 +3,8 @@ import Foundation
 extension PorscheConnect {
 
   public func checkStatus(
-    vehicle: Vehicle, capabilities: Capabilities? = nil,
+    vin: String,
+    capabilities: Capabilities? = nil,
     remoteCommand: RemoteCommandAccepted
   ) async throws -> (
     status: RemoteCommandStatus?, response: HTTPURLResponse?
@@ -12,8 +13,7 @@ extension PorscheConnect {
 
     let result = try await networkClient.get(
       RemoteCommandStatus.self,
-      url: urlForCommand(
-        vehicle: vehicle, capabilities: capabilities, remoteCommand: remoteCommand),
+      url: urlForCommand(vin: vin, capabilities: capabilities, remoteCommand: remoteCommand),
       headers: headers, jsonKeyDecodingStrategy: .useDefaultKeys)
     return (status: result.data, response: result.response)
   }
@@ -21,21 +21,21 @@ extension PorscheConnect {
   // MARK: - Private
 
   private func urlForCommand(
-    vehicle: Vehicle, capabilities: Capabilities?, remoteCommand: RemoteCommandAccepted
+    vin: String, capabilities: Capabilities?, remoteCommand: RemoteCommandAccepted
   ) -> URL {
     switch remoteCommand.remoteCommand {
     case .honkAndFlash:
       return networkRoutes.vehicleHonkAndFlashRemoteCommandStatusURL(
-        vin: vehicle.vin, remoteCommand: remoteCommand)
+        vin: vin, remoteCommand: remoteCommand)
     case .toggleDirectCharge:
       return networkRoutes.vehicleToggleDirectChargingRemoteCommandStatusURL(
-        vin: vehicle.vin, capabilities: capabilities, remoteCommand: remoteCommand)
+        vin: vin, capabilities: capabilities, remoteCommand: remoteCommand)
     case .toggleDirectClimatisation:
       return networkRoutes.vehicleToggleDirectClimatisationRemoteCommandStatusURL(
-        vin: vehicle.vin, remoteCommand: remoteCommand)
+        vin: vin, remoteCommand: remoteCommand)
     case .lock, .unlock:
       return networkRoutes.vehicleLockUnlockRemoteCommandStatusURL(
-        vin: vehicle.vin, remoteCommand: remoteCommand)
+        vin: vin, remoteCommand: remoteCommand)
     case .none:
       return URL(string: kBlankString)!
     }
